1f5299b240457cefecec0c7e95906c965b53664d
Josué Lima <josuedsi@gmail.com>
2014-11-20 01:24:38 -0300

    implementa cnpj e cpf

--- lib/act_as_cnpj_cpf/cnpj_ou_cpf.rb
+++ lib/act_as_cnpj_cpf/cnpj_ou_cpf.rb
@@ -1,10 +1,20 @@
 module ActAsCnpjCpf
   class CnpjOuCpf
+    extend Forwardable
+    # forward dos metodos e vars para o objeto criado de acordo
+    # com o numero passado
+    def_delegators :@instance, :numero, :to_s, :valido?, :formatado
 
+    # Infere se utiliza cnpj ou cpf pelo tamanho
+    # da string passada
+    #
+    # 11 = cpf | > 11 = cnpj
+    #
+    # Caso seja um numero invalido, o proprio objeto
+    # se encarrega da validacao.
     def initialize(numero)
-      klass = numero.to_s.gsub(/[^0-9]/, '').length == 11 ? Cpf : Cnpj
-      self  = klass.new(numero)
+      klass     = numero.to_s.gsub(/[^0-9]/, '').length == 11 ? Cpf : Cnpj
+      @instance = klass.new(numero)
     end
-
   end
 end
