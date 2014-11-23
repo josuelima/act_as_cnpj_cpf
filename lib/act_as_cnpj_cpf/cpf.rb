308ff7fb04a61254475a95c8a06697458b7bf4a9
Josué Lima <josuedsi@gmail.com>
2014-11-23 14:47:33 -0300

    remove unnecessary private const

--- lib/act_as_cnpj_cpf/cpf.rb
+++ lib/act_as_cnpj_cpf/cpf.rb
@@ -2,16 +2,14 @@ module ActAsCnpjCpf
   class Cpf
     include Base
 
+    LENGTH  = 11
+    REGEX   = /^(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})$/
+    ALGS_1  = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    ALGS_2  = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+
     def formatado
       @numero =~ /(\d{3})\.?(\d{3})\.?(\d{3})-?(\d{2})/
       valido? ? "#{$1}.#{$2}.#{$3}-#{$4}" : ''
     end
-
-    private
-
-      LENGTH  = 11
-      REGEX   = /^(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})$/
-      ALGS_1  = [10, 9, 8, 7, 6, 5, 4, 3, 2]
-      ALGS_2  = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
   end
 end
